0583e0fa6c0636da8e76a9ea61eda512eb7ac9b3
Yemekar <yemekar1999@gmail.com>
2024-12-02 11:11:39 +0000

    03_conversionandOperation

---
+++ 01_basics/03_conversionandOperation.js
@@ -0,0 +1,27 @@
+let score = "33abc"
+// console.log(score);
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+//  "33" => 33
+// "33asdf" => NaN
+// true => 1 ; false=> 0
+
+let isLoggedIn = 1
+let booleanisLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanisLoggedIn);
+// console.log(bpp;eanIsLoggedIn);
+// 1 => true; 0 => false
+//"" => false
+// "dy" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
