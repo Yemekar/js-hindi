a35e0fdb33d2653eeee2727e2cc7db580a896bf1
Yemekar <yemekar1999@gmail.com>
2024-12-03 18:41:12 +0000

    05_dataTypes-Summary.js

---
+++ 01_basics/05_dataTypes-Summary.js
@@ -0,0 +1,35 @@
+// primitive
+// 7 types : String , Number , Boolean, null, undefined, Symbol, BigInt
+// JavaScript dynamic typed or statically typed language? => dynamic typed
+const score = 200
+const scoreValue = 2.2
+ const isLoggedIn = false
+ const outsideTemp= null
+ let userEmail;
+const id =  Symbol('234')
+const anotherId = Symbol('234')
+// console.log(id=== anotherId);
+const bigNumber = 12325345236274575n
+
+
+
+
+
+
+// Reference (Non primitive)
+// Array, Objects, Functions 
+const heros = ["shaktiman", "nagraj", "doga"]
+const myObj = {
+    name: "dy",
+    age:24,
+}
+
+
+
+const myFunction =function(params) {
+    console.log("hello world");
+    
+}
+console.log(typeof bigNumber);
+console.log(outsideTemp);
+console.log(myFunction);
